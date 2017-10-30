236d4ee8a33a9fd90662970eea699f506db9a2e5
dima.dosta <dima.dosta9@gmail.com>
2017-10-30 13:04:12 +0300

    removed alert component

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,38 +1,28 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 import { Router, NavigationStart } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Subject } from 'rxjs/Subject';
 import { AlertTypes } from '../constants/alert.types';
 import { Message } from '../shared/models/index';
 
 
 @Injectable()
 export class AlertService {
-  private subject = new Subject<any>();
-  private keepAfterNavigationChange = false;
+  private config: MatSnackBarConfig;
 
-  constructor(private router: Router) {
-    // clear alert message on route change
-    router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        if (this.keepAfterNavigationChange) {
-          this.keepAfterNavigationChange = false;
-        }
-      }
-    });
+  constructor(
+    private router: Router,
+    public snackBar: MatSnackBar) {
+    this.config = new MatSnackBarConfig();
+    this.config.duration = 1500;
   }
 
-  public success(message: string, keepAfterNavigationChange = false): void {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
-    this.subject.next(new Message(AlertTypes.Success, message));
+  public success(message: string): void {
+    this.config.extraClasses = ['success'];
+    this.snackBar.open(message, undefined, this.config);
   }
 
-  public error(message: string, keepAfterNavigationChange = false): void {
-    this.keepAfterNavigationChange = keepAfterNavigationChange;
-    this.subject.next(new Message(AlertTypes.Error, message));
-  }
-
-  public getMessage(): Observable<Message> {
-    return this.subject.asObservable();
+  public error(message: string): void {
+    this.config.extraClasses = ['error'];
+    this.snackBar.open(message, undefined, this.config);
   }
 }
